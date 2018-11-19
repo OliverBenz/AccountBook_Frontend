eea21f4c32db4fc5712fdd358c29a78d0698d48e
OliverBenz <oliver.benz@outlook.com>
2018-11-19 09:45:03 +0100

    add account

--- src/app/add-account/add-account.component.ts
+++ src/app/add-account/add-account.component.ts
@@ -9,36 +9,6 @@ import { AccountService } from '../services/account.service';
   styleUrls: ['./add-account.component.css']
 })
 export class AddAccountComponent implements OnInit {
-  page = {
-    title: 'Add Account',
-    date: '',
-    username: {
-      title: 'Username *',
-      error:{
-        message: 'A valid username is required',
-        hidden: true
-      }
-    },
-    password: {
-      title: 'Password *',
-      error: {
-        message: 'Please enter a password',
-        hidden: true
-      }
-    },
-
-    site: {
-      title: 'Website *',
-      error: {
-        message: 'Please enter a website',
-        hidden: true
-      }
-    },
-    other: {
-      title: 'Other',
-      message: "Not required"
-    }
-  }
 
   accountHistory: Array<Account> = [];
 
@@ -52,54 +22,31 @@ export class AddAccountComponent implements OnInit {
   ngOnInit() {
   }
 
-  // TODO: Push data to database
-  // Send data to database by input over "Enter" or Button klick
-  SendAccount(username: string, password: string, info: string, website: string){
+  SendAccount(username: string, password: string, website: string, info: string){
+    var error = false;
 
     // Check if empty
-    if(website == ''){
-      this.page.site.error.hidden = false;
+    if(username == "" || password == "" || website == ""){
+      error = true;
     }
-    if(username == ''){
-      this.page.username.error.hidden = false;
-    }
-    if(password == ''){
-      this.page.password.error.hidden = false;
-    }
-
-
-    // Check if filled out again
-    if(username != ''){
-      this.page.username.error.hidden = true;
-    }
-    if(password != ''){
-      this.page.password.error.hidden = true;
-    }
-    if(website != ''){
-      this.page.site.error.hidden = true;
+  	
+    // Check if already added
+    for (let i = 0; i < this.accountHistory.length; i++){
+      // If account already posted - error
+      if(this.accountHistory[i].getUsername() == username && this.accountHistory[i].getPassword() == password && this.accountHistory[i].getWebsite() == website){
+        error = true;
+      }
     }
 
-    // OK - ready for push
-    if(username != '' && password != '' && website != ''){
+    if(error == false){
+      // Make account object and push to history
       var dateOptions = {day: 'numeric', month: 'numeric', year: 'numeric'};
       var date = new Date().toLocaleString('de-AU', dateOptions);
 
       let account: Account = new Account(0, website, username, password, date, info, 0, 0);
-
-      // 0 is OK; 1 is error
-      var error = "0";
-
-      // check for all accounts this user has posted
-      for (let i=0; i < this.accountHistory.length; i++){
-        // If account already posted - error
-        if(this.accountHistory[i].getUsername() == account.getUsername() && this.accountHistory[i].getPassword() == account.getPassword() && this.accountHistory[i].getWebsite() == account.getWebsite()){
-          error = "1";
-        }
-      }
-      if(error == "0"){
-        this.accountHistory.push(account);
-        this.accountService.sendAccounts(account);
-      }
+      
+      this.accountHistory.push(account);
+      this.accountService.sendAccounts(account);
     }
   }
 }
